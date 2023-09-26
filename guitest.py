4b0e5950cb9bad1ae880866eda80734449235f82
Atharva-2108 <117053490+Atharva-2108@users.noreply.github.com>
2023-09-26 22:43:36 +0530

    Initial commit

---
+++ guitest.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+while True:
+    pyautogui.keyDown('A')
+    pyautogui.keyDown('t')
+    pyautogui.keyDown('h')
+    pyautogui.keyDown('a')
+    pyautogui.keyDown('r')
+    pyautogui.keyDown('v')
+    pyautogui.keyDown('a')
+    pyautogui.keyDown(' ')
+    
